5abf67476d4a61bd8ec4f1aab6995889cf547b8b
nagito.terada <nagito.terada@hokuden-it.co.jp>
2022-08-12 16:39:40 +0900

    add react-router-dom.

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Environment } from "src/constants";
+import { Home } from "./pages";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={Environment.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ packages/web/src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Box, Button } from "@mui/material";
+
+export const Home: React.FC = () => {
+  return (
+    <Box>
+      <Button>Hoge</Button>
+    </Box>
+  );
+};
